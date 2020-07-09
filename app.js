9e308c6623251218b4668b3ab7c8ea164532a546
Ignacio Vissio <nachov663@gmail.com>
2020-07-09 15:28:47 -0300

    updated 09/07/2020

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 mongoose.connect('mongodb+srv://nachov301:Leavemealone3930@cluster0-ix6bg.mongodb.net/diep', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set("useCreateIndex", true);
 
 const app = express();
 
@@ -10,6 +14,32 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+app.use(session({
+  secret: "secret",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+const userSchema = new mongoose.Schema ({
+  user: String,
+  password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model("User", userSchema);
+
+// use static authenticate method of model in LocalStrategy
+passport.use(User.createStrategy());
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 const postSchema = {
   title: String,
@@ -26,61 +56,182 @@ const Post = mongoose.model("Post", postSchema);
 
 // ===========================================GET requests=========================================================
 
+app.get("/signUp", function(req, res){
+  res.render("signUp");
+});
+
 app.get("/", function(req, res){
+  res.render("login", {message: "Enter your user and password"});
+});
+
+app.get("/failureLogin", function(req, res){
+  res.render("login", {message: "User or password incorrect, try again :("});
+});
+
+app.get("/home", function(req, res){
+
+if(req.isAuthenticated()){
   Post.find({}, function(err, foundPost){
     if(err){
       console.log(err);
     }else{
-      res.render("index", {posts:foundPost});
+      res.render("home", {posts:foundPost});
     }
   });
+}else{
+  res.redirect("/signUp");
+}
+
+
+});
+
+app.get("/diep", function(req, res){
+
+  if(req.isAuthenticated){
+    Post.find({}, function(err, foundPost){
+      if(err){
+        console.log(err);
+      }else{
+        res.render("index", {posts:foundPost});
+      }
+    });
+  }else{
+    res.redirect("/");
+  }
+
+
 });
 
 app.get("/37826537", function(req, res){
-  res.render("compose");
+
+  if(req.isAuthenticated){
+    res.render("compose");
+  }else{
+    res.redirect("/");
+  }
+
+
 });
 
 app.get("/timeline", function(req, res){
-  Post.find({timeline: "checked"}, function(err, foundPost){
-    if(err){
-      console.log(err);
-    }else{
-      res.render("categories", {title: "Timeline Posts", posts:foundPost});
-    }
-  });
+
+  if(req.isAuthenticated){
+    Post.find({timeline: "checked"}, function(err, foundPost){
+      if(err){
+        console.log(err);
+      }else{
+        res.render("categories", {title: "Timeline Posts", posts:foundPost});
+      }
+    });
+  }else{
+    res.redirect("/");
+  }
+
+
 });
 
 app.get("/hand", function(req, res){
-  Post.find({hand: "checked"}, function(err, foundPost){
-    if(err){
-      console.log(err);
-    }else{
-      res.render("categories", {title: "Hand Posts", posts:foundPost});
-    }
-  });
+
+  if(req.isAuthenticated){
+    Post.find({hand: "checked"}, function(err, foundPost){
+      if(err){
+        console.log(err);
+      }else{
+        res.render("categories", {title: "Hand Posts", posts:foundPost});
+      }
+    });
+  }else{
+    res.redirect("/");
+  }
+
+
 });
 
 app.get("/thoughts", function(req, res){
-  Post.find({thoughts: "checked"}, function(err, foundPost){
-    if(err){
-      console.log(err);
-    }else{
-      res.render("categories", {title: "Thoughts Posts", posts:foundPost});
-    }
-  });
+
+  if(req.isAuthenticated){
+    Post.find({thoughts: "checked"}, function(err, foundPost){
+      if(err){
+        console.log(err);
+      }else{
+        res.render("categories", {title: "Thoughts Posts", posts:foundPost});
+      }
+    });
+  }else{
+    res.redirect("/");
+  }
+
+
 });
 
 app.get("/journey", function(req, res){
-  Post.find({journey: "checked"}, function(err, foundPost){
-    if(err){
-      console.log(err);
-    }else{
-      res.render("categories", {title: "Journey Posts", posts:foundPost});
-    }
-  });
+
+  if(req.isAuthenticated){
+    Post.find({journey: "checked"}, function(err, foundPost){
+      if(err){
+        console.log(err);
+      }else{
+        res.render("categories", {title: "Journey Posts", posts:foundPost});
+      }
+    });
+  }else{
+    res.redirect("/");
+  }
+
+
 });
 
 //=============================================POST request========================================================
+
+app.post("/logOut", function(req, res){
+  req.logout();
+  res.redirect("/");
+});
+
+app.post("/signUp", function(req, res){
+  res.redirect("/signUp");
+});
+
+app.post('/login',
+  passport.authenticate('local', { successRedirect: '/home',
+                                   failureRedirect: '/failureLogin' }));
+// In this case, the redirect options override the default behavior. Upon successful authentication,
+// the user will be redirected to the home page. If authentication fails, the user will be redirected
+// back to the login page for another attempt.
+
+app.post("/register", function(req, res){
+
+const username = req.body.username;
+const password = req.body.password;
+const confirmPassword = req.body.confirmPassword;
+
+console.log(password);
+console.log(confirmPassword);
+
+if(password === confirmPassword){
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    console.log(req.body.username);
+    if(err){
+      console.log(err);
+      console.log("error");
+      res.redirect("/");
+    }else{
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/home");
+      });
+    }
+
+  });
+}else{
+  res.redirect("signUp");
+}
+
+
+});
+
+app.post("signUp", function(req, res){
+  res.redirect("/signUp");
+});
 
 app.post("/timeline", function(req, res){
   res.redirect("/timeline");
@@ -142,7 +293,7 @@ app.post("/compose", function(req, res){
 
   newPost.save();
 
-  res.redirect("/");
+  res.redirect("/diep");
 
 });
 
